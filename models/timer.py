48cd4bf0f4775a48f4adca481baa7d71bab6c984
Bridgit <bridgichepkwony3@gmail.com>
2024-08-19 11:25:06 +0300

    creating the timer object to check on the time of the tasks

--- models/timer.py
+++ models/timer.py
@@ -1,18 +1,89 @@
 #!/usr/bin/python3
-import models
-from models.base_model import Base, BaseModel
-from sqlalchemy import Column, String, Integer, ForeignKey, Text, Enum as SqlEnum, DateTime
-from sqlalchemy.orm import relationship
-from models.enumerations import TaskStatus
 from datetime import datetime
+from sqlalchemy import Column, String, ForeignKey, DateTime, Integer
+from models.base_model import Base, BaseModel
+import models
 
 
 class Timer(BaseModel, Base):
     """
-    CONSTRUCTS THE CLASS TIMER
+    Timer class to track the duration of tasks
     """
-    __tablename__ = "timers"
+    __tablename__ = 'timers'
 
-    start_time = Column(DateTime, default=datetime.utcnow, nullable=False)
+    task_id = Column(String(60), ForeignKey('tasks.id'), nullable=False)
+    start_time = Column(DateTime, nullable=False, default=datetime.utcnow)
     end_time = Column(DateTime, nullable=True)
-    
+
+    @property
+    def duration(self):
+        """
+        Calculate the duration in seconds
+        """
+        if self.end_time:
+            return (self.end_time - self.start_time).total_seconds()
+        return None
+
+    def start(self):
+        """
+        Start the timer by setting the start_time to the current time
+        """
+        self.start_time = datetime.utcnow()
+        models.storage.save()
+
+    def stop(self):
+        """
+        Stop the timer by setting the end_time to the current time
+        """
+        self.end_time = datetime.utcnow()
+        models.storage.save()
+
+    def reset(self):
+        """
+        Reset the timer by clearing start_time and end_time
+        """
+        self.start_time = datetime.utcnow()
+        self.end_time = None
+        models.storage.save()
+
+
+class SubtaskTimer(BaseModel, Base):
+    """
+    Timer class to track the duration of subtasks
+    """
+    __tablename__ = 'subtask_timers'
+
+    subtask_id = Column(String(60), ForeignKey('subtasks.id'), nullable=False)
+    start_time = Column(DateTime, nullable=False, default=datetime.utcnow)
+    end_time = Column(DateTime, nullable=True)
+
+    @property
+    def duration(self):
+        """
+        Calculate the duration in seconds
+        """
+        if self.end_time:
+            return (self.end_time - self.start_time).total_seconds()
+        return None
+
+    def start(self):
+        """
+        Start the timer by setting the start_time to the current time
+        """
+        self.start_time = datetime.utcnow()
+        models.storage.save()
+
+    def stop(self):
+        """
+        Stop the timer by setting the end_time to the current time
+        """
+        self.end_time = datetime.utcnow()
+        models.storage.save()
+
+    def reset(self):
+        """
+        Reset the timer by clearing start_time and end_time
+        """
+        self.start_time = datetime.utcnow()
+        self.end_time = None
+        models.storage.save()
